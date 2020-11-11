579134bb991d90e855413817f47f1e6886481ad1
Allief Nuriman <57051116+allief876@users.noreply.github.com>
2020-11-11 20:18:36 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template
+import prosesinput
+import cosines
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('index.html') #render template di homepage.html
+
+@app.route('/index.html')
+def home2():
+	return render_template('index.html') #render template di homepage.html
+
 @app.route('/aboutus.html') # Pindah dari localhost:5000 ke localhost:5000/about
 def about():
 	return render_template('aboutus.html')
+
 @app.route('/howtouse.html')
 def howuse():
 	return render_template('howtouse.html')
-@app.route('/logo.jpg')
-def pics():
-	return render_template('logo.jpg')
+
+@app.route('/', methods=['GET'])
+def my_form_post():
+    return prosesinput('GET')
+
+@app.route('/index.html', methods=['GET']) # Backup kalo user nginput di /index.html
+def my_form_post2():
+    return prosesinput('GET')
+
 if __name__ == "__main__":
 	app.run(debug=True)
