8a4c41bebba91c8186ce6180b01f98f035723a10
Uyamikun <nabil.nabighah@gmail.com>
2020-11-11 20:20:37 +0700

    vektor dan cossim

---
+++ src/vector.py
@@ -0,0 +1,22 @@
+import math
+
+def sim(q,d): #ngitung similarity dari query sama document  
+    dot = dotprod(q,d)
+    magq = magvec(q,q)
+    magd = magvec(q,d)
+    kalimag = (magq*magd)
+    return dot/kalimag
+
+def dotprod(q,d): #sudah benar
+    r = 0
+    for i in q:
+        x = d.get(i,0)
+        r += q[i]*x
+    return r
+
+def magvec(q,v): #besar vector, x = query atau dictionary
+    sum = 0
+    for i in q:
+        x = v.get(i,0)
+        sum += x**2 
+    return sum**0.5
