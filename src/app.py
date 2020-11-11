09fcd83b42dc71e91eaa73398ea75ce57e7fb8e6
Allief Nuriman <57051116+allief876@users.noreply.github.com>
2020-11-11 20:56:05 +0700

    Minor changes in app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-import prosesinput
-import cosines
+from flask import Flask, render_template, request, redirect, url_for
+import readfile
+import vector
 
 app = Flask(__name__)
 
@@ -12,21 +12,28 @@ def home():
 def home2():
 	return render_template('index.html') #render template di homepage.html
 
-@app.route('/aboutus.html') # Pindah dari localhost:5000 ke localhost:5000/about
+@app.route('/aboutus.html') # Pindah dari localhost:5000 ke localhost:5000/aboutus.html
 def about():
 	return render_template('aboutus.html')
 
-@app.route('/howtouse.html')
+@app.route('/howtouse.html')  # Pindah dari localhost:5000 ke localhost:5000/howtouse.html
 def howuse():
 	return render_template('howtouse.html')
 
 @app.route('/', methods=['GET'])
-def my_form_post():
+def keyboard_input():
     return prosesinput('GET')
 
 @app.route('/index.html', methods=['GET']) # Backup kalo user nginput di /index.html
-def my_form_post2():
+def keyboard_input():
     return prosesinput('GET')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
 	app.run(debug=True)
